676ee63afbb8f6af850333055ce2dca1f5ceaa55
Ioulianos Kakoulidis <ioulianos.kakoulidis@hotmail.com>
2021-08-16 10:15:27 +0300

    DJBHash: loop sentinel

--- src/DJBHash.cpp
+++ src/DJBHash.cpp
@@ -1,19 +1,21 @@
 /**
    @file DJBHash.cpp
    uCDB standard hash function implementation
-   
+
    Created by Ioulianos Kakoulidis, 2021.
-   Released into the public domain.   
+   Released into the public domain.
 */
 
 #include "DJBHash.h"
 
 unsigned long DJBHash(const void *key, unsigned long keyLen) {
   unsigned long h = 5381;
-  const byte *k = (const byte *)key;
+  const byte *curr = (const byte *)key;
+  const byte *end = curr + keyLen;
 
-  for (unsigned long i = 0; i < keyLen; i++) {
-    h = ((h << 5) + h) ^ k[i];
+  while (curr < end) {
+    h = ((h << 5) + h) ^ *curr;
+    ++curr;
   }
 
   return h;
