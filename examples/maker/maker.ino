53ef255c7532490863c354bb2b29e9b1ae3115c1
Ioulianos Kakoulidis <ioulianos.kakoulidis@hotmail.com>
2022-01-21 09:41:57 +0200

    maker example

--- examples/maker/maker.ino
+++ examples/maker/maker.ino
@@ -11,32 +11,18 @@
   Created by Ioulianos Kakoulidis, 2022.
   Released into the public domain.     
 */
-//#include <SPI.h>
-//#include <SD.h>
 
 #include "SdFat.h"
 
 #define SD_CS_PIN 10
-/*
-#define ENABLE_DEDICATED_SPI 1
-
-// Try to select the best SD card configuration.
-#if HAS_SDIO_CLASS
-#define SD_CONFIG SdioConfig(FIFO_SDIO)
-#elif  ENABLE_DEDICATED_SPI
-#define SD_CONFIG SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SPI_HALF_SPEED )
-#else  // HAS_SDIO_CLASS
-#define SD_CONFIG SdSpiConfig(SD_CS_PIN, SHARED_SPI, SPI_HALF_SPEED )
-#endif  // HAS_SDIO_CLASS
-*/
 
 #define TRACE_CDB
 #define USE_UCDB_MAKER
 #include "uCDB.hpp"
 
-SdFat fat;
-uCDBMaker<SdFat, File> maker(fat);
-uCDB<SdFat, File> ucdb(fat);
+SdFat32 fat;
+uCDBMaker<SdFat32, File32> maker(fat);
+uCDB<SdFat32, File32> ucdb(fat);
 
 unsigned long startMillis;
 
@@ -86,27 +72,29 @@ void setup() {
 
   Serial.begin(115200);
   while (!Serial) {
-    ;
+    yield();
   }
 
-  // Initialize the SD.
   if (!fat.begin(SD_CS_PIN)) {
     fat.initErrorHalt(&Serial);
     return;
   }
   
+  Serial.println("Creating CDB file - testCDB...\nRange [0, 100000), step - 5.\nPlease wait!");
   rt = maker.init("testCDB");
-  Serial.println(rt);
-  Serial.print("Records number: "); Serial.println(maker.recordsNumber());  
+  
+  if (rt == CDB_ERROR) {
+    Serial.println("Init() error");
+    return;    
+  }
   
   startMillis = millis();
   for (unsigned long i = 0; i < 100000; i += 5) {
     sprintf(key, "%lu", i);
     rt = maker.appendKeyValue(key, strlen(key), key, strlen(key));
     if (rt == CDB_ERROR) {
-      Serial.println(i);    
-      Serial.println(rt);    
-      break;
+      Serial.print("appendKeyValue error: "); Serial.println(key);
+      return;
     }
   }
   Serial.print("Records number: "); Serial.println(maker.recordsNumber());
@@ -116,16 +104,16 @@ void setup() {
   rt = maker.finalize();
   Serial.println(millis() - startMillis);  
   Serial.println(rt);    
-
+  Serial.println("testCDB created.");    
+  
   if (ucdb.open("testCDB.cdb") == CDB_OK) {
     Serial.print("Total records number: ");
     Serial.println(ucdb.recordsNumber());
   }
   else {
-    Serial.print("Invalid CDB: ");
-    Serial.println("testCDB.cdb");
+    Serial.println("Invalid CDB: testCDB.cdb");
+    return;
   }
-  
 }
 
 void loop() {
